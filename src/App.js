fdbecf70bf1e55b68ef548cfce049c1b6dc03060
TashaRolland92 <tasharolland@hotmail.co.uk>
2024-11-11 13:26:54 +0000

    finished UI | added some accessibility attribs

--- src/App.js
+++ src/App.js
@@ -24,30 +24,29 @@ function App() {
 	};
 
 	return (
-		<div className="App container mx-auto">
-			<header className="w-full flex justify-between mb-4">
+		<div className="App container max-w-screen-xl mx-auto px-4">
+			<header className="w-full flex flex-col justify-center lg:flex-row lg:justify-between items-center mb-4">
 				<h1 className="
 					logo
-					py-6
+					pt-6 pb-0
 					font-anton
-					inline-block
-					bg-gradient-to-r from-secondary-content via-secondary-color to-secondary-dark
-					bg-clip-text
+					colour-transparent inline-block bg-gradient-to-r from-primary-content to-secondary-content bg-clip-text
 					text-6xl
+					whitespace-nowrap
 					text-transparent
 					">
 					T-iMDB</h1>
 				<img
 					src="/images/squad1.webp"
-					alt="Deadshot, Harley Quinn & Joker. Suicide Squad Character Street Art."
-					className="relative top-0 right-0 -z-1 squad-image w-3/4"
+					alt="Deadshot, Harlequinn & Joker. Suicide Squad Character Street Art."
+					className="relative top-0 right-0 -z-1 squad-image"
 				/>
 			</header>
-			<section className="intro mb-4">
-				<p className="text-primary-content text-lg pb-2">Use the search below to find out more information about your favourite movies!</p>
+			<section className="intro text-center lg:text-left mb-4">
+				<p className="text-primary-content text-lg pb-2">Use the search below to find out more information on your favourite movies!</p>
 			</section>
 
-			<form onSubmit={handleSubmit} className="bg-transparent">
+			<form onSubmit={handleSubmit} className="bg-transparent" autoComplete="off">
 				<div className="bg-transparent">
 					<input
 						type="text"
@@ -59,21 +58,34 @@ function App() {
 							text-primary-content
 							text-lg
 							rounded-3xl
-							min-h-12 min-w-96
-							px-4
+							min-h-12
+							px-4 mb-4
 							opacity-100
-							shadow shadow-white
-							focus:border-primary-content focus:outline-none focus:shadow-lg"
+							shadow-lg shadow-white
+							w-full lg:w-96
+							focus:border-primary-content focus:outline-none focus:shadow-white"
 						placeholder="Enter movie title..."
 						value={searchTerm}
 						onChange={(e) => setSearchTerm(e.target.value)}
 					/>
-					<button type="submit" className="uppercase rounded-3xl bg-secondary-color text-md text-secondary-content hover:bg-secondary-light transition ease duration-150 min-h-12 px-2 min-w-32 ml-4">Search</button>
+					<button type="submit" aria-label="Search button" className="
+						uppercase
+						rounded-3xl
+						bg-secondary-color
+						text-md
+						text-secondary-content
+						hover:bg-secondary-light
+						transition ease duration-300
+						min-h-12
+						px-2
+						w-full lg:w-48
+						focus:border-0 focus:outline-none
+						lg:ml-4">Search</button>
 				</div>
 			</form>
 
 			{results && results.Response === "True" ? (
-				<div className="results mt-10 pt-10 grid grid-cols-2 border-t-2 border-secondary-content">
+				<div className="results mt-10 pt-10 grid md:grid-cols-2 border-t-2 border-secondary-content">
 					<div className="movie-details-container mr-2">
 						<h2 className="text-primary-content font-anton text-3xl mb-4 underline">{ results.Title}</h2>
 						<ul className="movie-details text-primary-content text-lg mb-4">
@@ -87,7 +99,7 @@ function App() {
 							<li><span className="font-bold">iMDB Votes:</span> <span className="text-secondary-content">{results.imdbVotes}</span></li>
 						</ul>
 						<h3 className="font-anton text-secondary-content underline text-2xl mb-4">Other Ratings</h3>
-						<ul className="other-ratings text-primary-content text-lg">
+						<ul className="other-ratings text-primary-content text-lg mb-4">
 							{results.Ratings && results.Ratings.length > 0 ? (
 								results.Ratings.map((rating, index) => (
 									<li key={index}>
@@ -99,8 +111,8 @@ function App() {
 							)}
 						</ul>
 					</div>
-					<div className="poster-plot-container flex flex-col align-end mb-6">
-						<img src={results.Poster} alt={results.Title} className="mb-4" />
+					<div className="poster-plot-container flex flex-col mb-6">
+						<img src={results.Poster} alt={results.Title} className="mb-4 l" />
 						<div className="plot">
 							<h3 className="font-anton text-secondary-content text-2xl mb-4 underline">Synopsis</h3>
 							<p class="text-secondary-content text-lg">{results.Plot}</p>
@@ -109,7 +121,16 @@ function App() {
 				</div>
 			)
 			: (
-				results && <p>{results.Error}</p> // error message from ODMB API
+				results &&
+					<div className="flex items-center">
+						<h4 className="mt-4 mx-auto flex
+							font-anton text-xl uppercase
+							text-primary-content
+							whitespace-nowrap
+							text-transparent">{results.Error}
+							{/* // error message from ODMB API */}
+						</h4>
+					</div>
 			)}
 		</div>
 	);
